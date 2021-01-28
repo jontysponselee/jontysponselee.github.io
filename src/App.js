803e64ec1067d7b2a0e9db56fd8064ccbdf756b5
Jonty Sponselee <jsponselee97@gmail.com>
2021-01-28 22:59:01 +0100

    Feat: add routing for pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
+import {BrowserRouter, Switch, Route, Redirect, NavLink} from "react-router-dom";
+
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/projects">Projects</NavLink>
+            <NavLink to="/toolkit">Toolkit</NavLink>
+            <NavLink to="/about-me">About me</NavLink>
+            <NavLink to="/contact-me">Contact me</NavLink>
+
+            <Switch>
+                <Route exact path={"/"}>
+                    <p>homepage</p>
+                </Route>
+                <Route exact path={"/projects"}>
+                    <p>projects</p>
+                </Route>
+                <Route exact path={"/toolkit"}>
+                    <p>toolkit</p>
+                </Route>
+                <Route exact path={"/about-me"}>
+                    <p>About me</p>
+                </Route>
+                <Route exact path={"/contact-me"}>
+                    <p>Contact me</p>
+                </Route>
+                <Route path={"*"}>
+                    <Redirect to={"/"}/>
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    );
 }
 
 export default App;
